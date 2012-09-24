7c016659a806379c206f05f1ef6793f5f4609a32
Ryan Hewitt <rthewitt.devel@gmail.com>
2012-09-23 17:56:22 -0700

    eliminated infinite loop, compromise until daemon established

--- thalamus.py
+++ thalamus.py
@@ -22,10 +22,10 @@ conn.connect()
 
 conn.subscribe(destination='/queue/test', ack='auto')
 
-#time.sleep(20)
+time.sleep(2000)
 
 # work on daemon...
-while True:
-   pass
+#while True:
+#   pass
 
 conn.disconnect()
